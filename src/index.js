75d959ab06f7fd4a5231863bc509dcad105e2571
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2019-07-31 22:13:00 -0700

    Add Login component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Login from './components/login.jsx'
 
 const App = () => {
-  return <div>Hello React,Webpack 4, Babel 7!</div>;
+  return <div>Hello React,Webpack 4, Babel 7!
+    <Login/>
+  </div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
