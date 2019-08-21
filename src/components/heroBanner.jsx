6c7a6fbacc008b4a8ca36d616b069761970b4f88
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2019-08-20 18:21:41 -0700

    Create react-webpack-babel template with simple express server

--- src/components/heroBanner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function HeroBanner(props){
-  return (
-    <div className="jumbotron jumbotron-fluid" style={{backgroundImage: "url(images/banner.jpg)", backgroundSize: "cover"}}>
-      <h1 className="display-1" style={{color: "white", textAlign: "center", textShadow: "0px 0px 5px black"}}>Antioch Wesleyan Church</h1>
-    </div>
-  )
-}
--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-
-import HeroBanner from "./components/heroBanner.jsx"
-import NavBar from "./components/navBar.jsx";
-import ContactInfo from "./components/contactInfo.jsx"
-import AboutUs from "./components/aboutUs.jsx";
-import SizedPhotoViewer from "./components/carousel/sizedPhotoViewer.jsx"
-import Calendar from "./components/calendar/calendar.jsx";
-import SermonPlayer from "./components/sermonPlayer/sermonPlayer.jsx"
+import Welcome from './components/welcome.jsx'
 
 const App = () => {
-  const [page, setPage] = useState("home")
 
-  function renderPage(){
-    switch(page){
-      case 'home':
-        return <ContactInfo/>
-      case 'about':
-        return <AboutUs/>
-      case 'calendar':
-        return <Calendar/>
-      case 'pictures':
-        return <SizedPhotoViewer/>
-      case 'sermons':
-        return <SermonPlayer/>
-      default:
-        return <ContactInfo/>
-    }
-  }
-
-  return <div className="container">
-     <HeroBanner src="./images/banner.jpg"/>
-     <NavBar setPage={setPage}/>
-     <hr/>
-     {renderPage()}
+  return <div>
+      <Welcome/>
     </div>;
 };
 
